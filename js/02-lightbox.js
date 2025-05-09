21b6dddff54b2e7722ed3efe07f2cf2b127a0543
Andrii Shkliar <elswortz@gmail.com>
2025-05-09 20:17:39 +0300

    add completed 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListRef = document.querySelector(".gallery");
+
+const galleryMarkup = CreateGalleryMarkup(galleryItems);
+galleryListRef.innerHTML = galleryMarkup;
+
+function CreateGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+      </a>
+    </li>
+    `;
+    })
+    .join("");
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "top",
+  captionDelay: 250,
+});
+lightbox.open();
