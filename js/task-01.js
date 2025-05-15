2214279519b6a12426aeb73d52003a863c429d15
Andrii Shkliar <elswortz@gmail.com>
2025-05-15 17:50:26 +0300

    clear project

--- js/task-01.js
+++
@@ -1,9 +0,0 @@
-const listRef = document.querySelector("#categories");
-
-console.log(`Number of categories: ${listRef.children.length}`);
-
-[...listRef.children].forEach((item) => {
-  console.log(
-    `Category: ${item.firstElementChild.textContent}\nElements: ${item.lastElementChild.children.length}\n`
-  );
-});
--- js/task-07.js
+++
@@ -1,9 +0,0 @@
-const inputRef = document.querySelector("#font-size-control");
-const spanRef = document.querySelector("#text");
-
-inputRef.addEventListener("input", onInputChange);
-
-function onInputChange(event) {
-  const input = event.currentTarget;
-  spanRef.style.fontSize = `${input.value}px`;
-}
