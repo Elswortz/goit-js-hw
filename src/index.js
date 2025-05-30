67ef81b16e5ae55204acbd6964b3792d80e92a7f
Andrii Shkliar <elswortz@gmail.com>
2025-05-30 23:06:14 +0300

    add all completed tasks of module 10

--- src/index.js
+++ src/index.js
@@ -1,19 +1,80 @@
-import axios from 'axios';
-import { fetchBreeds } from './api/cat-api';
+import { CatsApiService } from './api/cat-api';
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
 
-axios.defaults.headers.common['x-api-key'] =
-  'live_KTQhLGn5Bf5conehSP2WoCD89idQkFHCviZNt2YTmd5ZGF6nLKZV9sMeIXQ33urg';
-
+const API = new CatsApiService();
 const selectRef = document.querySelector('.breed-select');
+const infoBlockRef = document.querySelector('.cat-info');
+const loaderRef = document.querySelector('.loader');
+const errorRef = document.querySelector('.error');
 
-fetchBreeds()
-  .then(cats => {
-    console.log(cats);
-    const markup = cats
-      .map(({ id, name }) => {
-        return `<option value="${id}">${name}</option>`;
-      })
-      .join();
-    selectRef.innerHTML = markup;
-  })
-  .catch(error => console.log(error));
+errorRef.hidden = true;
+
+renderSelectOptions();
+selectRef.addEventListener('change', onSelectChange);
+
+function onSelectChange(event) {
+  errorRef.hidden = true;
+  showLoader();
+  const catId = event.currentTarget.value;
+
+  infoBlockRef.innerHTML = '';
+
+  Promise.all([API.fetchCatImageByBreed(catId), API.fetchBreedById(catId)])
+    .then(([catImage, catInfo]) => {
+      hideLoader();
+      renderCatImage(catImage[0].url);
+      renderCatInfo(catInfo);
+    })
+    .catch(error => {
+      hideLoader();
+      errorRef.hidden = false;
+      Notiflix.Notify.failure(`${error}`);
+    });
+}
+
+function renderSelectOptions() {
+  showLoader();
+  selectRef.hidden = true;
+  API.fetchBreeds()
+    .then(cats => {
+      const optionsMarkup = cats.map(
+        ({ id, name }) => `<option value="${id}">${name}</option>`
+      );
+      selectRef.insertAdjacentHTML('beforeend', optionsMarkup.join(''));
+
+      var select = new SlimSelect({
+        select: '#slimselect',
+      });
+
+      selectRef.hidden = false;
+      hideLoader();
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(`${error}`);
+    });
+}
+
+function renderCatImage(url) {
+  const markup = `<div class="img-block"><img src="${url}" alt="Cat"></div>`;
+  infoBlockRef.insertAdjacentHTML('beforeend', markup);
+}
+
+function renderCatInfo({ name, description, temperament }) {
+  const markup = `
+  <div class="info-block">
+  <h2 class="Cat__Title">${name}</h2>
+  <p class="Cat__Description">${description}</p>
+  <p class="Cat__Temperamnet">${temperament}</p>
+  </div>
+  `;
+  infoBlockRef.insertAdjacentHTML('beforeend', markup);
+}
+
+function showLoader() {
+  loaderRef.classList.add('is-visible');
+}
+
+function hideLoader() {
+  loaderRef.classList.remove('is-visible');
+}
